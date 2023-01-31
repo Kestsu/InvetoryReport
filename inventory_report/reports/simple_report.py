748bddf379d650b3bd15a6405cedbc2e37138eaf
Kevin Li <Kevinstart14@gmail.com>
2023-01-31 19:56:33 -0300

    feito 3

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -36,41 +36,42 @@ class SimpleReport(ABC):
         for item in lista_de_empresa:
             contagem[item] = contagem.get(item, 0) + 1
 
+        # print(contagem)
         empresa_mais_repetida = max(contagem, key=contagem.get)
 
         return f"""Data de fabricação mais antiga: {data_mais_antiga}
 Data de validade mais próxima: {data_validade_perto}
 Empresa com mais produtos: {empresa_mais_repetida}"""
 
-testss = SimpleReport.generate(   [
-     {
-       "id": 1,
-       "nome_do_produto": "CADEIRA",
-       "nome_da_empresa": "Forces of Nature",
-       "data_de_fabricacao": "2022-04-01",
-       "data_de_validade": "2021-02-07",
-       "numero_de_serie": "FR48",
-       "instrucoes_de_armazenamento": "Conservar em local fresco"
-     },
-          {
-       "id": 1,
-       "nome_do_produto": "CADEIRA",
-       "nome_da_empresa": "Forces of Nature",
-       "data_de_fabricacao": "2022-04-04",
-       "data_de_validade": "2023-02-03",
-       "numero_de_serie": "FR48",
-       "instrucoes_de_armazenamento": "Conservar em local fresco"
-     },
-          {
-       "id": 1,
-       "nome_do_produto": "CADEIRA",
-       "nome_da_empresa": "Forces of Nature",
-       "data_de_fabricacao": "2022-04-02",
-       "data_de_validade": "2023-02-01",
-       "numero_de_serie": "FR48",
-       "instrucoes_de_armazenamento": "Conservar em local fresco"
-     }
-   ])
+# testss = SimpleReport.generate(   [
+#      {
+#        "id": 1,
+#        "nome_do_produto": "CADEIRA",
+#        "nome_da_empresa": "Forces of Nature",
+#        "data_de_fabricacao": "2022-04-01",
+#        "data_de_validade": "2021-02-07",
+#        "numero_de_serie": "FR48",
+#        "instrucoes_de_armazenamento": "Conservar em local fresco"
+#      },
+#           {
+#        "id": 1,
+#        "nome_do_produto": "CADEIRA",
+#        "nome_da_empresa": "Forces of Nature",
+#        "data_de_fabricacao": "2022-04-04",
+#        "data_de_validade": "2023-02-03",
+#        "numero_de_serie": "FR48",
+#        "instrucoes_de_armazenamento": "Conservar em local fresco"
+#      },
+#           {
+#        "id": 1,
+#        "nome_do_produto": "CADEIRA",
+#        "nome_da_empresa": "Forces of",
+#        "data_de_fabricacao": "2022-04-02",
+#        "data_de_validade": "2023-02-01",
+#        "numero_de_serie": "FR48",
+#        "instrucoes_de_armazenamento": "Conservar em local fresco"
+#      }
+#    ])
 
-print(testss)
+# print(testss)
 
