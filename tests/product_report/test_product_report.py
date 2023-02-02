5f19d7bfc27033b0e852d0a8d4142b8f92583a6d
Kevin Li <Kevinstart14@gmail.com>
2023-02-01 23:26:57 -0300

    feito 8

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,21 +1,20 @@
 from inventory_report.inventory.product import Product
 
 
-def __repr__():
-    pass
-
 def test_relatorio_produto():
-    
-    product_instance = Product(  
-    1,
-    "CADEIRA",
-    "Forces of Nature",
-    "2022-04-04",
-    "2023-02-09",
-    "FR48",
-    "Conservar em local fresco",
-    )
 
-    assert product_instance == (f"O produto CADEIRA fabricado em 2022-04-04 por"
-    f" Forces of Nature com validade até 2023-02-09"
-    f" precisa ser armazenado Conservar em local fresco.")
+    product_instance = Product(
+        1,
+        "CADEIRA",
+        "Forces of Nature",
+        "2022-04-04",
+        "2023-02-09",
+        "FR48",
+        "Conservar em local fresco",
+        )
+
+    assert product_instance.__repr__() == (
+        "O produto CADEIRA fabricado em 2022-04-04 " +
+        "por Forces of Nature com validade até 2023-02-09 " +
+        "precisa ser armazenado Conservar em local fresco."
+    )
