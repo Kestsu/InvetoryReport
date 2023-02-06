34c3d5e84cb74247b913a98b94eadfc4cee73c22
Kevin Li <Kevinstart14@gmail.com>
2023-02-06 15:39:31 -0300

    corrigindo erros de lint no completeReport

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,36 +1,22 @@
-# from simple_report import SimpleReport
 from inventory_report.reports.simple_report import SimpleReport
 from abc import ABC
-import json
+
 
 class CompleteReport(ABC):
     def generate(list: list[dict]):
         simple_report = SimpleReport.generate(list)
 
         lista_de_empresa = [
-             empresas["nome_da_empresa"] 
+            empresas["nome_da_empresa"]
             for empresas in list
         ]
 
         contagem = {}
         for item in lista_de_empresa:
             contagem[item] = contagem.get(item, 0) + 1
-        
+
         string = "Produtos estocados por empresa:"
         for item in contagem.items():
             string += "\n" + f"- {item[0]}: {item[1]}"
-        
-       
 
         return simple_report + "\n" + string + "\n"
-
-# with open("inventory_report/data/inventory.json") as json_file:
-#     dados = json.load(json_file)
-
-#     testss = CompleteReport.generate(
-#     dados
-#     )
-
-#     print(testss)
-
-#  ✗ python3 inventory_report/reports/complete_report.py
